13437cb86d00c6019f7f5407f92d07a3e4c85cf8
kalelchaves <kalel.branco@gmail.com>
2015-09-08 20:41:53 -0300

    fix nomes dos arquivos para compatibilidade com apache e composer autoload

---
+++ vendor/SON/Di/Container.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SON\Di;
+
+class Container {
+
+    public static function getClass($name) {
+        $str_class = "\\App\\Models\\" . ucfirst($name);
+
+        $class = new $str_class(\App\business::getDb());
+
+        return $class;
+    }
+
+    public static function trataData($data) {
+        if (strpos($data, '/') > 0) {
+            $resultado = implode('-', array_reverse(explode('/', $data)));
+        } else {
+            $resultado = implode('/', array_reverse(explode('-', $data)));
+        }
+
+        return $resultado;
+    }
+
+}
