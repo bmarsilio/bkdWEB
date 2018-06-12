95e61a02c33c22ac6e08e3e3f17e20257e0e4d83
bmarsilio <bruno.marsilio@hotmail.com>
2018-06-11 22:11:24 -0300

    teste

--- Public/include.php
+++ Public/include.php
@@ -1,11 +1,11 @@
-<?php  
+<?php
 	session_start();
 	error_reporting(E_ALL^E_NOTICE^E_DEPRECATED);
 	//session_destroy();
 	//unset($_SESSION);
 	//die(var_dump($_SESSION[logado]));
 	//$_SESSION[logado] = true;
-	if(!$_SESSION[logado]){
+	if(!$_SESSION['logado']){
 		header('Location: login');
 	}
 ?>
